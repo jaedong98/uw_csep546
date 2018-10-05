24c1a0c30d543aaba29e92c08542fc308383d4f6
Jae Dong Hwang <jaedong@uw.edu>
2018-10-05 16:32:02 -0700

    UPDATED: logistic regression model.

--- Assignment1/Code/LogisticRegressionModel.py
+++ Assignment1/Code/LogisticRegressionModel.py
@@ -4,17 +4,17 @@ import math
 class LogisticRegressionModel(object):
     """A logistic regression spam filter"""
 
-    def __init__(self):
-        pass
+    def __init__(self, threshold=0.5):
+        self.threshold = threshold
 
     def fit(self, xTrain, yTrain, iterations, step=0.01):
         self.weights = [.75, .75, .75, .25, .25]
         print("Fitting training dataset with {} iteration".format(iterations))
         print("Initial: {}".format(self.weights))
         while iterations > 0:
-            yTrainPredicted = self.predict(xTrain)
+            yTrainPredicted = self.predict_prob(xTrain)
             for i in range(len(self.weights)):
-                partial_loss = 0
+                partial_loss = 0.0
                 j = 0
                 for x, yhat_j, y_j in zip(xTrain, yTrainPredicted, yTrain):
                     partial_loss += (yhat_j - y_j) * x[i]
@@ -29,13 +29,13 @@ class LogisticRegressionModel(object):
 
     def loss(self, xTest, yTest):
 
-        yTestPredicted = self.predict(xTest)
+        yTestPredicted = self.predict_prob(xTest)
         loss = 0
         for y_hat, y in zip(yTestPredicted, yTest):
             loss += (-y * math.log(y_hat)) - ((1 - y) * (math.log(1.0-y_hat)))
         return loss
 
-    def predict(self, x):
+    def predict_prob(self, x):
 
         predictions = []
 
@@ -45,9 +45,22 @@ class LogisticRegressionModel(object):
             z = self.weights[0] + sum(scores)
             prediction = 1.0 / (1.0 + math.exp(-z))
             predictions.append(prediction)
-            # if prediction > 0.5:
-            #    predictions.append(1)
-            # else:
-            #    predictions.append(0)
+
+        return predictions
+
+    def predict(self, x):
+
+        predictions = self.predict_prob(x)
+        
+        return [1 if x > self.threshold else 0 for x in predictions]
+        for example in x:
+            scores = [example[i] * self.weights[i]
+                      for i in range(len(example))]
+            z = self.weights[0] + sum(scores)
+            prediction = 1.0 / (1.0 + math.exp(-z))
+            if prediction > 0.5:
+                predictions.append(1)
+            else:
+                predictions.append(0)
 
         return predictions
--- Assignment1/Code/StartingPoint1.py
+++ Assignment1/Code/StartingPoint1.py
@@ -5,7 +5,7 @@ import EvaluationsStub
 # UPDATE this path for your environment
 import os
 kDataPath = os.path.join(os.path.dirname(
-    os.path.dirname(__file__)), r"Data\SMSSpamCollection")
+    os.path.dirname(__file__)), r"Data/SMSSpamCollection")
 
 (xRaw, yRaw) = Assignment1Support.LoadRawData(kDataPath)
 
@@ -43,13 +43,28 @@ EvaluationsStub.ExecuteAll(yTest, yTestPredicted)
 ############################
 import LogisticRegressionModel
 model = LogisticRegressionModel.LogisticRegressionModel()
-
-print("Logistic regression model")
-for i in [10, 20, 30]:
+print("#############################")
+print("### Logistic regression model")
+training_set_loss_vs_iterations = []
+test_set_loss_vs_iterations = []
+test_set_accuracy_vs_iterations = []
+w1_vs_iterations = []
+for i in [100, 200, 300, 400, 500]:
     model.fit(xTrain, yTrain, iterations=i, step=0.01)
+    training_set_loss_vs_iterations.append((model.loss(xTrain, yTrain), i))
+
     yTestPredicted = model.predict(xTest)
+    test_set_loss_vs_iterations.append((model.loss(xTest, yTest), i))
+    test_set_accuracy_vs_iterations.append((EvaluationsStub.Accuracy(yTest, yTestPredicted), i))
+    w1_vs_iterations.append((model.weights[1], i))
 
     print("%d, %f, %f, %f" % (i, model.weights[1], model.loss(
           xTest, yTest), EvaluationsStub.Accuracy(yTest, yTestPredicted)))
 
-#EvaluationsStub.ExecuteAll(yTest, yTestPredicted)
+print("Training set loss vs iterations: {}".format(training_set_loss_vs_iterations))
+print("Test set loss vs iterations: {}".format(test_set_loss_vs_iterations))
+print("Test set accuracy vs. iterations: {}".format(test_set_accuracy_vs_iterations))
+print("W1 vs. iterations: {}".format(w1_vs_iterations))
+
+print("++++++++++++++++++++++++++++++++")
+EvaluationsStub.ExecuteAll(yTest, yTestPredicted)
