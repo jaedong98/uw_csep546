7f4385c872a614d4a13905e180a30c97a499b23b
jaedong hwang <jaedong.hwang@boeing.com>
2018-10-11 11:13:49 -0700

    IMPLEMENTED: mi2 method (this is the right direction).

--- Assignment2/Code/features_by_mi.py
+++ Assignment2/Code/features_by_mi.py
@@ -76,7 +76,42 @@ def calculate_mi(y0_counter, y1_counter, top=10):
     return mi.most_common(top)
 
 
-features = calculate_mi(y0_counter, y1_counter, top=10)
+def calculate_mi2(y0_counter, y1_counter, top=10):
+
+    features = y0_counter.keys()
+    y0_N = sum(y0_counter.values())
+    y1_N = sum(y1_counter.values())
+    N = y0_N + y1_N
+    mi = collections.Counter()
+
+    for f in features:
+        if not f:
+            continue
+        # For 'Call'
+
+        #          |   'call'  | no 'Call' | ...
+        # |--------|-----------|-----------|...
+        # | y(=1)  |    n11    |    n10    |...
+        # | y(=0)  |    n01    |    n00    |...
+        n11 = y1_counter[f]         # number of 'Call' when y = 1
+        n10 = y1_N - y1_counter[f]  # number of no 'Call' when y = 1
+        n01 = y0_counter[f]         # number of 'Call' when y = 0
+        n00 = y0_N - y0_counter[f]  # number of no 'Call' when y = 0
+        
+        n = n00 + n01 + n10 + n11
+        n1_ = n10 + n11
+        n_1 = n01 + n11
+        n0_ = n00 + n01
+        n_0 = n00 + n10
+        
+        mi[f] = (n11/n) * math.log2((n*n11 + 1) / (n1_ * n_1))\
+                + (n01/n) * math.log2((n*n01 + 1) / (n0_ * n_1))\
+                + (n10/n) * math.log2((n*n10 + 1) / (n1_ * n_0))\
+                + (n00/n) * math.log2((n*n00 + 1) / (n0_ * n_0))
+
+    return mi.most_common(top)
+
+features = calculate_mi2(y0_counter, y1_counter, top=10)
 # print_lookup_table(y0_counter, y1_counter)
 
 # Create a lookup table
