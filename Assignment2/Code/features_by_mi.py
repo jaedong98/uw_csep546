9323ca9a02860222649c0d6ce47f1853e41f3d3b
jaedong hwang <jaedong.hwang@boeing.com>
2018-10-11 10:16:50 -0700

    UPDATED: mi calulation based on the equation in https://nlp.stanford.edu/IR-book/html/htmledition/mutual-information-1.html

--- Assignment2/Code/features_by_mi.py
+++ Assignment2/Code/features_by_mi.py
@@ -26,16 +26,7 @@ N = 10
 (xTrainRaw, yTrainRaw, xTestRaw,
  yTestRaw) = utils.TrainTestSplit(xRaw, yRaw)
 
-feature_selection_methods = [utils.is_longger,
-                             utils.has_number,
-                             utils.contain_call,
-                             utils.contain_to,
-                             utils.contain_your]
-
-xTrain = utils.FeaturizeTraining(xTrainRaw, feature_selection_methods)
-
-
-## MI calculation
+# MI calculation
 
 y0_counter = collections.Counter()
 y1_counter = collections.Counter()
@@ -49,46 +40,50 @@ for y, x in zip(yTrainRaw, xTrainRaw):
             y1_counter[w] += 1
 
 if not len(y0_counter.keys()) == len(y1_counter.keys()):
-    raise ValueError('Missing keys() {} vs {}'.format(len(y0_counter.keys()),len(y1_counter.keys())))
+    raise ValueError('Missing keys() {} vs {}'.format(
+        len(y0_counter.keys()), len(y1_counter.keys())))
 
 
 def calculate_mi(y0_counter, y1_counter, top=10):
 
     features = y0_counter.keys()
     y0_N = sum(y0_counter.values())
-    y1_N = sum(y1_counter.values()) 
+    y1_N = sum(y1_counter.values())
     N = y0_N + y1_N
-
     mi = collections.Counter()
 
-
     for f in features:
+
+        if not f:
+            continue
 
         p_f_0 = (y0_counter[f] + 1) / (N + 2)
         p_0 = (y0_counter[f] + 1) / (y0_N + 2)
         total_cnt = y0_counter[f] + y1_counter[f]
         p_f = (y0_counter[f] + 1) / (total_cnt + 2)
-        mi_0 = p_f_0 * math.log2(p_f_0 / (p_f * p_0))
+        #mi_0 = p_f_0 * math.log2(p_f_0 / (p_f * p_0))
+        mi_0 = p_f_0 * math.log2((N * y0_counter[f] + 1) / ((y0_N)*(total_cnt) * 2))
 
         p_f_1 = (y1_counter[f] + 1) / (N + 2)
         p_1 = (y1_counter[f] + 1) / (y1_N + 2)
         total_cnt = y0_counter[f] + y1_counter[f]
-        p_f = (y0_counter[f] + 1) / (total_cnt + 2)
-        mi_1 = p_f_1 * math.log2(p_f_1 / (p_f * p_1))
-
+        p_f = (y1_counter[f] + 1) / (total_cnt + 2)
+        #mi_1 = p_f_1 * math.log2(p_f_1 / (p_f * p_1))
+        mi_1 = p_f_1 * math.log2((N * y1_counter[f] + 1) / ((y1_N)*(total_cnt) * 2))
 
         mi[f] = mi_0 + mi_1
 
     return mi.most_common(top)
 
+
 features = calculate_mi(y0_counter, y1_counter, top=10)
 # print_lookup_table(y0_counter, y1_counter)
 
 # Create a lookup table
-#         |   'call'  |   'OK'   | ...
-#|--------|-----------|----------|...
-#| y(=0)  |     5     |    2     |...
-#| y(=1)  |     3     |    1     |...
+#          |   'call'  |   'OK'   | ...
+# |--------|-----------|----------|...
+# | y(=0)  |     5     |    2     |...
+# | y(=1)  |     3     |    1     |...
 
 # note from Geoff:
 # p(call, 0) would be 5 / 11 (= 5 / N)
@@ -96,7 +91,23 @@ features = calculate_mi(y0_counter, y1_counter, top=10)
 # p(call) would be 5/8
 # but then smooth them by adding 1 to the numerator and 2 to the denominator
 
-table = utils.selected_features_table(features, ["Features", "Mutual Information"], w=25)
+# then
+# p(call, 1) would be 3 / 11 (= 3 / N)
+# p(1) would be 3/4
+# p(call) would be 3/8
+
+#          |   'call'  |   'OK'   | ...
+# |--------|-----------|----------|...
+# | y(=0)  |     5     |    0     |...
+# | y(=1)  |     0     |    5     |...
+# p(call, 0) would be 5 / 10 (= 5 / N)
+# p(0) would be 5/5
+# p(call) would be 5/5
+
+
+
+table = utils.selected_features_table(
+    features, ["Features", "Mutual Information"], w=25)
 
 table_md = os.path.join(report_path, 'features_selected_by_mi.md')
 
