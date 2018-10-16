d6e4f16fef28c568b05b5f870400c37fa4eefc94
jaedong hwang <jaedong.hwang@boeing.com>
2018-10-16 07:02:48 -0700

    UPDATED: to generate FPR and FNR plots.

--- Assignment3/Code/precision_vs_recall.py
+++ Assignment3/Code/precision_vs_recall.py
@@ -19,14 +19,12 @@ report_path = os.path.join(os.path.dirname(
     os.path.dirname(__file__)), r"Report")
 
 
-
-
 def run_logistic_regression_w_threshold(xTrainRaw, xTestRaw, yTrain, yTest, N=10,
-                                     max_iters=50000, iter_step=1000, step=0.01,
-                                     initial_w0=0.0, report_path=report_path,
-                                     predict_thresholds=[0.5],
-                                     additional_features=[]):
-    
+                                        max_iters=50000, iter_step=1000, step=0.01,
+                                        initial_w0=0.0, report_path=report_path,
+                                        predict_thresholds=[0.5],
+                                        additional_features=[]):
+
     # features by MI
     features, _ = fbm.extract_features_by_mi(xTrainRaw, yTrainRaw, N)
 
@@ -41,21 +39,22 @@ def run_logistic_regression_w_threshold(xTrainRaw, xTestRaw, yTrain, yTest, N=10
     xTest = utils.FeaturizeTrainingByWords(xTestRaw, features)
     xTest = [[1] + x for x in xTest]
     model = utils.logistic_regression_by_features(xTrainRaw, xTestRaw,
-                                                      yTrain, yTest,
-                                                      features, iter_step,
-                                                      resolution, initial_w0,
-                                                      step, max_iters,
-                                                      return_model=True)
+                                                  yTrain, yTest,
+                                                  features, iter_step,
+                                                  resolution, initial_w0,
+                                                  step, max_iters,
+                                                  return_model=True)
     precisions_vs_recalls = []
-    thresholds_fp_fn = []
+    thresholds_fp = []
+    thresholds_fn = []
     for predict_threshold in predict_thresholds:
-        
+
         yTestPredicted = model.predict(xTest, predict_threshold)
         evaluation = EvaluationsStub.Evaluation(yTest, yTestPredicted)
         precisions_vs_recalls.append((evaluation.recall, evaluation.precision))
-        thresholds_fp_fn.append((predict_threshold, evaluation.fpr, evaluation.fnr))
-    return precisions_vs_recalls, thresholds_fp_fn
-
+        thresholds_fp.append((predict_threshold, evaluation.fpr))
+        thresholds_fn.append((predict_threshold, evaluation.fnr))
+    return precisions_vs_recalls, thresholds_fp, thresholds_fn
 
 
 if __name__ == '__main__':
@@ -67,36 +66,75 @@ if __name__ == '__main__':
      yTestRaw) = utils.TrainTestSplit(xRaw, yRaw)
     yTrain = yTrainRaw
     yTest = yTestRaw
-    predict_thresholds = list(np.linspace(0.01, 0.99, 101))
+
+    N = 10
     max_iters = 50000
     iter_step = 1000
     step = 0.01
     initial_w0 = 0.0
-    N = 10
-
+    predict_thresholds = list(np.linspace(0.01, 0.99, 101))
+    additional_features = ['call', 'to', 'your']
     ##
-    prs1, fp_fn1 = run_logistic_regression_w_threshold(xTrainRaw, xTestRaw, yTrain, yTest, N=N,
-                                        max_iters=max_iters, iter_step=iter_step, step=step,
-                                        initial_w0=initial_w0,
-                                        predict_thresholds=predict_thresholds)
-    csv_fname = os.path.join(report_path, 'precision_vs_recall_{}_thresholds.csv'.format(max_iters))
+    prs1, fp1, fn1 = run_logistic_regression_w_threshold(
+        xTrainRaw, xTestRaw, yTrain, yTest,
+        N=N,
+        max_iters=max_iters,
+        iter_step=iter_step,
+        step=step,
+        initial_w0=initial_w0,
+        predict_thresholds=predict_thresholds)
+    csv_fname = os.path.join(
+        report_path, 'precision_vs_recall_{}_thresholds.csv'.format(max_iters))
     utils.write_csv(prs1, csv_fname, headers=["Recall", "Precision"])
-    csv_fname = os.path.join(report_path, 'thresholds_fpr_fnr_{}_thresholds.csv'.format(max_iters))
-    utils.write_csv(fp_fn1, csv_fname, headers=["Thresholds", "False Positive Rate", "False Negative Rate"])
-
+    csv_fname = os.path.join(
+        report_path, 'thresholds_fp_{}.csv'.format(max_iters))
+    utils.write_csv(fp1, csv_fname, headers=[
+                    "Thresholds", "False Positive Rates"])
+    csv_fname = os.path.join(
+        report_path, 'thresholds_fn_{}.csv'.format(max_iters))
+    utils.write_csv(fn1, csv_fname, headers=[
+                    "Thresholds", "False Negative Rates"])
     ##
-    prs2, fp_fn2 = run_logistic_regression_w_threshold(xTrainRaw, xTestRaw, yTrain, yTest, N=N,
-                                        max_iters=max_iters, iter_step=iter_step, step=step,
-                                        initial_w0=initial_w0,
-                                        predict_thresholds=predict_thresholds,
-                                        additional_features=['call', 'to', 'your'])
-    csv_fname = os.path.join(report_path, 'precision_vs_recall_{}_thresholds_w_additional.csv'.format(len(predict_thresholds)))
+    prs2, fp2, fn2 = run_logistic_regression_w_threshold(
+        xTrainRaw, xTestRaw, yTrain, yTest,
+        N=N,
+        max_iters=max_iters,
+        iter_step=iter_step,
+        step=step,
+        initial_w0=initial_w0,
+        predict_thresholds=predict_thresholds,
+        additional_features=additional_features)
+    csv_fname = os.path.join(
+        report_path, 'precision_vs_recall_{}_thresholds_w_additional.csv'.format(len(predict_thresholds)))
     utils.write_csv(prs2, csv_fname, headers=["Recall", "Precision"])
-    csv_fname = os.path.join(report_path, 'thresholds_fpr_fnr_{}_thresholds_w_additional.csv'.format(max_iters))
-    utils.write_csv(fp_fn1, csv_fname, headers=["Thresholds", "False Positive Rate", "False Negative Rate"])
+    csv_fname = os.path.join(
+        report_path, 'thresholds_fp_{}_w_additional.csv'.format(max_iters))
+    utils.write_csv(fp2, csv_fname, headers=[
+                    "Thresholds", "False Positive Rates"])
+    csv_fname = os.path.join(
+        report_path, 'thresholds_fn_{}_w_additional.csv'.format(max_iters))
+    utils.write_csv(fn2, csv_fname, headers=[
+                    "Thresholds", "False Negative Rates"])
 
+    # problem 1 part 1.
     title = "Precision vs. Recalls"
     legends = ['Top 10 MI', 'Top 10 MI with +3']
     fname = 'precision_vs_recall_{}.png'.format(max_iters)
     img_fname = os.path.join(report_path, fname)
-    utils.draw_comparison([prs1, prs2], "Recalls", "Precisions", title, img_fname, legends)
+    utils.draw_comparison([prs1, prs2], "Recalls",
+                          "Precisions", title, img_fname, legends)
+
+    # problem 1 part2.
+    title = "Thresholds vs. FPRs"
+    legends = ['Top 10 MI', 'Top 10 MI with +3']
+    fname = 'thresholds_fpr_comp_{}.png'.format(max_iters)
+    img_fname = os.path.join(report_path, fname)
+    utils.draw_comparison([fp1, fp2], "Thresholds",
+                          "False Positive Rates", title, img_fname, legends)
+
+    title = "Thresholds vs. FNRs"
+    legends = ['Top 10 MI', 'Top 10 MI with +3']
+    fname = 'thresholds_fnr_comp_{}.png'.format(max_iters)
+    img_fname = os.path.join(report_path, fname)
+    utils.draw_comparison([fn1, fn2], "Thresholds",
+                          "False Negative Rates", title, img_fname, legends)
--- Assignment3/Report/prod1_roc_operating_points.md
+++ Assignment3/Report/prod1_roc_operating_points.md
@@ -2,9 +2,19 @@
 
 ## Jae Dong Hwang
 
-### Produce a chart that compares a:
+### PR Curve
 
- * logistic regression model with 10 mutual information features
- * logistic regression model with 10 mutual information features and my heuristic features
+#### Produce a chart that compares a:
+
+* logistic regression model with 10 mutual information features
+* logistic regression model with 10 mutual information features and my heuristic features
 
 ![poc](precision_vs_recall_50000.png)
+
+### FPR and FNR
+
+#### Hand in a table that contains the threshold that achieve 10% False Positive Rate on the test set for these two model (10 mutual information features with and without heuristics). Include the False Negative Rate that is achieved on the test set by that threshold.
+
+![fpr_comp](thresholds_fpr_comp_50000.png)
+
+![fnr_comp](thresholds_fnr_comp_50000.png)
