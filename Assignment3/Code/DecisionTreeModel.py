156e6b2f1009d85af0a4fe59ce157a413dbb8bff
Jae Dong Hwang <jaedong@uw.edu>
2018-10-17 16:15:05 -0700

    CREATED: DecisionTreeModel module.

---
+++ Assignment3/Code/DecisionTreeModel.py
@@ -0,0 +1,14 @@
+
+class DecisionTree():
+
+    def __init__(self, minToSplit=100):
+        self.minToSplit = minToSplit
+    
+    def fit(self, x, y):
+        pass
+
+    def predict(self, xTest):
+        pass
+
+    def visualize(self):
+        pass
