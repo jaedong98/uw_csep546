71e2e47619d5c424c558ce7bd6aa5d3d4b27ffc2
Jae Dong Hwang <jaedong@uw.edu>
2018-10-28 14:03:19 -0700

    ADDED: test accuracy results with feature restriction 20

--- Assignment4/Code/test/test_random_forest.py
+++ Assignment4/Code/test/test_random_forest.py
@@ -49,6 +49,7 @@ class TestRandomForestModel(unittest.TestCase):
 
     def test_accuracy_baseline_wo_noise_no_bootstrap(self):
         """
+        * feature_restriction = 0 (with all features)
         |          |    1     |    0     |
         |----------|----------|----------|
         |    1     | (TP) 180 | (FN) 22  |
@@ -58,6 +59,17 @@ class TestRandomForestModel(unittest.TestCase):
         Recall: 0.8910891089108911
         FPR: 0.015939597315436243
         FNR: 0.10891089108910891
+
+        * feature_restriction = 20
+        |          |    1     |    0     |
+        |----------|----------|----------|
+        |    1     | (TP) 87  | (FN) 115 |
+        |    0     |  (FP) 0  |(TN) 1192 |
+        Accuracy: 0.9175035868005739
+        Precision: 1.0
+        Recall: 0.4306930693069307
+        FPR: 0.0
+        FNR: 0.5693069306930693
         :return:
         """
         xTrain, xTest, yTrain, yTest = get_featurized_xs_ys(numMutualInformationWords=295,
@@ -65,7 +77,7 @@ class TestRandomForestModel(unittest.TestCase):
 
         rfm = RandomForestModel(numTrees=10,
                                 bagging_w_replacement=False,
-                                feature_restriction=0,
+                                feature_restriction=20,
                                 seed=100)
         rfm.fit(xTrain, yTrain, min_to_split=2)
         yTestPredicted = rfm.predict(xTest)
@@ -76,6 +88,7 @@ class TestRandomForestModel(unittest.TestCase):
 
     def test_accuracy_baseline_wo_noise_w_bootstrap(self):
         """
+        * feature_restriction = 0 (with all features)
         |          |    1     |    0     |
         |----------|----------|----------|
         |    1     |  (TP) 2  | (FN) 200 |
@@ -85,6 +98,17 @@ class TestRandomForestModel(unittest.TestCase):
         Recall: 0.009900990099009901
         FPR: 0.007550335570469799
         FNR: 0.9900990099009901
+
+        * feature_restriction = 20
+        |          |    1     |    0     |
+        |----------|----------|----------|
+        |    1     |  (TP) 0  | (FN) 202 |
+        |    0     |  (FP) 0  |(TN) 1192 |
+        Accuracy: 0.8550932568149211
+        Precision: 0.0
+        Recall: 0.0
+        FPR: 0.0
+        FNR: 1.0
         :return:
         """
         xTrain, xTest, yTrain, yTest = get_featurized_xs_ys(numMutualInformationWords=295,
@@ -92,7 +116,7 @@ class TestRandomForestModel(unittest.TestCase):
 
         rfm = RandomForestModel(numTrees=10,
                                 bagging_w_replacement=True,
-                                feature_restriction=0,
+                                feature_restriction=20,
                                 seed=100)
         rfm.fit(xTrain, yTrain, min_to_split=2)
         yTestPredicted = rfm.predict(xTest)
