8460fbe02b9f866b025795b6bd42937d9f39c88b
Jae Dong Hwang <jaedong@uw.edu>
2018-10-31 16:47:12 -0700

    IMPLEMENTED: gradient calculation and unittest.

--- Assignment5/Code/test/test_cv_features.py
+++ Assignment5/Code/test/test_cv_features.py
@@ -4,7 +4,7 @@ import unittest
 import PIL
 from Assignment5.Code import kDataPath
 from PIL import Image
-from utils.cv_features import divide_image
+from utils.cv_features import divide_image, get_x_gradients, get_x_gradient_features, get_y_gradient_features
 
 
 class TestCVFeatures(unittest.TestCase):
@@ -33,7 +33,19 @@ class TestCVFeatures(unittest.TestCase):
             plt.imshow(grids[i - 1])
         plt.show()
 
+    def test_get_x_gradients(self):
+        fname = os.path.join(kDataPath, r'closedRightEyes/closed_eye_0001.jpg_face_1_R.jpg')
+        image = Image.open(fname, 'r')
+        grids = divide_image(image, grid_dim=(3, 3))
+        for grid in grids:
+            x_grads = get_x_gradients(grid)
+            self.assertTrue(len(x_grads) == 64)
 
+        features = get_x_gradient_features(image, grid_dim=(3, 3))
+        self.assertTrue(len(features) == 27)
+
+        features = get_y_gradient_features(image, grid_dim=(3, 3))
+        self.assertTrue(len(features) == 27)
 
 
 if __name__ == '__main__':
--- Assignment5/Code/utils/cv_features.py
+++ Assignment5/Code/utils/cv_features.py
@@ -2,6 +2,29 @@ import numpy as np
 
 
 def divide_image(image, grid_dim=(3, 3)):
+    """
+    Divide image into N x M grid
+    :param image:
+    :param grid_dim:
+    :return: a list of numpy arrays
+
+    For example, for 24 x 24 pixel image, if griding it into 3 x 3,
+    this function returns a list of arrays below.
+
+    | ------------- | --------------| ------------- |
+    | 3 x 3 Array 1 | 3 x 3 Array 2 | 3 x 3 Array 3 |
+    | ------------- | ----------- --| ------------- |
+    | 3 x 3 Array 4 | 3 x 3 Array 5 | 3 x 3 Array 6 |
+    | ------------- | ----------- --| ------------- |
+    | 3 x 3 Array 7 | 3 x 3 Array 8 | 3 x 3 Array 9 |
+    | ------------- | --------------| ------------- |
+
+    returns...
+    | ------------- | --------------|...|...| ------------- |
+    | 3 x 3 Array 1 | 3 x 3 Array 2 |...|...| 3 x 3 Array 9 |
+    | ------------- | ----------- --|...|...| ------------- |
+
+    """
     pixels = image.load()
     xSize = image.size[0]
     ySize = image.size[1]
@@ -42,7 +65,6 @@ def get_pixels(flattened, xs, ys, xSize):
     grid = []
     for y in ys:
         for x in xs:
-            #index = (y * width) + x
             index = (y * xSize) + x
             grid.append(flattened[index])
 
@@ -56,5 +78,55 @@ def flatten_image(pixels, xSize, ySize):
             flattened.append(pixels[x, y])
 
     return np.array(flattened)
+
+
+def get_x_gradients(grid):
+
+    columns, rows = grid.shape
+    gradients = []
+    for row in range(rows):
+        intensities = [c / 255. for c in grid[row, :]]
+        for i, column in enumerate(intensities):
+            if i == 0:
+                gradients.append(intensities[1] - intensities[0])
+            elif i == (columns - 1):
+                gradients.append(intensities[i] - intensities[i - 1])
+            else:
+                gradients.append(intensities[i + 1] - intensities[i - 1])
+
+    return gradients
+
+
+def get_x_gradient_features(image, grid_dim=(3, 3)):
+    """
+
+    :param image: an instance of PIL.Image.Image object.
+    :param grid_dim:
+    :return: a list of gradient min, max, average features of each grid.
+    """
+    grids = divide_image(image, grid_dim)
+    features = []
+    for grid in grids:
+        gradients = get_x_gradients(grid)
+        features.append(min(gradients))
+        features.append(max(gradients))
+        features.append(sum(gradients) / len(gradients))
+
+    return features
+
+
+def get_y_gradient_features(image, grid_dim=(3, 3)):
+    """
+
+    :param image: an instance of PIL.Image.Image object.
+    :param grid_dim:
+    :return: a list of gradient min, max, average features of each grid.
+    """
+    image = image.rotate(90)
+    return get_x_gradient_features(image, grid_dim)
+
+
+
+
 
 
