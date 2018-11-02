07d8e4e299974cd553c4f695ac084625fbf3f629
Jae Dong Hwang <jaedong@uw.edu>
2018-11-02 16:36:06 -0700

    REMOVED: pkls block push to origin. old prediction.

--- Assignment5/Code/model/KnnModel.py
+++ Assignment5/Code/model/KnnModel.py
@@ -1,9 +1,4 @@
 import collections as col
-import hashlib
-import numpy as np
-import os
-import pickle
-from model import cache_dir
 from sklearn.neighbors.kd_tree import KDTree
 
 runtime_data = {}
@@ -15,67 +10,6 @@ class KNearestNeighborModel(object):
         self.xTrains = xTrains
         self.yTrains = yTrains
         self.tree = KDTree(self.xTrains, leaf_size=2)
-
-
-    def get_ordered_train_neighbors(self, xTests):
-
-        hash_str = '{}_{}'.format(self.xTrains, xTests).encode()
-        knn_hash = hashlib.sha256(hash_str).hexdigest()
-        knn_pkl = os.path.join(cache_dir, '{}.pkl'.format(knn_hash))
-        if os.path.exists(knn_pkl):
-            neighbors_ordered = runtime_data.get(knn_hash, None)
-            if not neighbors_ordered:
-                print("Found pickled xTrains sorted.")
-                with open(knn_pkl, 'rb') as f:
-                    neighbors_ordered = pickle.load(f)
-                    runtime_data[knn_hash] = neighbors_ordered
-            return neighbors_ordered
-
-        neighbors_ordered = []
-        for xTest in xTests:
-
-            distances = []
-            for xt, yt in zip(self.xTrains, self.yTrains):
-                a1 = np.array(xTest)
-                a2 = np.array(xt)
-                d = np.linalg.norm(a1 - a2)
-                if d == 0:
-                    continue
-                distances.append((d, xt, yt))
-
-            neighbors_ordered.append(sorted(distances))
-
-        KNearestNeighborModel.runtime_data[knn_hash] = neighbors_ordered
-        with open(knn_pkl, 'wb') as f:
-            pickle.dump(neighbors_ordered, f)
-            print("Saved neighbors ordered in pickle {}".format(knn_pkl))
-
-        return neighbors_ordered
-
-    def predict_old(self, xTests, k, threshold=None):
-
-        if k > len(self.xTrains):
-            k = len(self.xTrains)
-
-        predictions = []
-        neighbors_ordered = self.get_ordered_train_neighbors(xTests)
-        for neighbors in neighbors_ordered:
-            k_neighbors = neighbors[:k]
-
-            k_predictions = [sd[-1] for sd in k_neighbors]
-
-            if threshold is not None:
-                cnt_1 = k_predictions.count(1)
-                proposition = cnt_1 / k
-                if proposition >= threshold:
-                    predictions.append(1)
-                else:
-                    predictions.append(0)
-            else:
-                mc = col.Counter(k_predictions).most_common(1)[0][0]
-                predictions.append(mc)
-
-        return predictions
 
     def predict(self, xTests, k, threshold=None):
 
