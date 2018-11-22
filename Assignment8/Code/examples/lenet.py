88d419df8dcebd60f325f0e446b513ec8e373e61
Jae Dong Hwang <jaedong@uw.edu>
2018-11-22 14:47:06 -0800

    CHANGED: 95.13% Accuracy simple: 0.9513201320132013, loss: 35.232.

--- Assignment8/Code/examples/lenet.py
+++ Assignment8/Code/examples/lenet.py
@@ -45,8 +45,7 @@ class LeNet(nn.Module):
     def forward(self, x):
         x = F.max_pool2d(self.conv1(x), (2, 2), stride=2)
         x = F.max_pool2d(self.conv2(x), (2, 2), stride=2)
-        x = x.view(-1, self.num_flat_features(x))
-        #x = x.reshape(x.size(0), -1)
+        x = x.reshape(x.size(0), -1)
         x = self.fc1(x)
         # x = self.fc2(x)
         x = self.fc3(x)
